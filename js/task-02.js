cc4bc88cd58c460a084a16911dbc152b84f31764
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-04 17:24:37 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.getElementById("ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.textContent = ingredient;
+  return listEl;
+});
+
+ingredientsEl.append(...elements);
