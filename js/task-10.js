90046deba722489a76d4d1db4f952790a90fd34e
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-05 17:03:17 +0300

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
+const boxes = document.getElementById("boxes");
+const input = document.querySelector("input");
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+
+// create
+
+create.addEventListener("click", onButtonCreateClick);
+
+function onButtonCreateClick() {
+  let divAmaunt = input.value;
+  createBoxes(divAmaunt);
+}
+
+function createBoxes(amount) {
+  let divArr = [];
+  for (let i = 0; i < amount; i += 1) {
+    // создаем div
+    let div = document.createElement("div");
+    // добавляем рандомный цвет
+    div.style.backgroundColor = getRandomHexColor();
+    // задаем размер
+    let size = 30 + i * 10;
+    div.style.width = size + "px";
+    div.style.height = size + "px";
+    // пуш
+    divArr.push(div);
+  }
+  boxes.append(...divArr);
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// destroy
+
+destroy.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
