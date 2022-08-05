b1263a199eba48f0215d4f0aee93fb766e2a592f
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-05 14:32:06 +0300

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", () => {
+  const color = (body.style.backgroundColor = getRandomHexColor());
+  span.textContent = color;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
